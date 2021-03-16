a62883e0f66e2c3d09d9c59e9e89cb3039e2663f
Nitesh Khatri <niteshkhatri545@gmail.com>
2021-03-16 13:19:56 +0530

    added changes

--- server.js
+++ server.js
@@ -1,59 +1,90 @@
-const express = require('express');
-const hbs = require('hbs');
-const fs=require('fs');
-var app = express();
+const path = require("path");
+var expr = require("express");
+const hbs = require("hbs");
+const fs = require('fs')
 
-const port=process.env.PORT || 3000;
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
+var app = expr();
 
-/*app.use((req,res,next)=>{
-	console.log("Running");
-	//next();    });*/
-app.use((req,res,next)=>{
-	var log=`${req.method} ${req.url}`;
-	console.log(log);
-	fs.appendFile('somefile.log',log+'\n',(err)=>
-	{		if(err)
-		{			console.log("Some Problem");	} });
-	next();	});
+const port = process.env.PORT || 3000;
 
-/*app.use((req,res,next)=>
-{	res.render('notify.hbs');
-	//next();	});*/
-app.use(express.static(__dirname + '/public'));
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear() });
 
-hbs.registerHelper('Capt', (text) => {
-  return text.toUpperCase();	});
+//const pubpath = path.join(__dirname, "./template");
+const path1 = path.join(__dirname, "./template/views");
+const path2 = path.join(__dirname, "./template/partials");
 
-app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
+
+app.set("view engine", "hbs");
+app.set("views", path1);
+
+hbs.registerPartials(path2);
+
+//app.use(expr.static(pubpath));
+
+app.use((req, res, next) => {
+  var log = `${req.method} ${req.url} ${req.on}`;
+  
+  fs.appendFile('somefile.log', log + "\n", (err) => {
+    if (err) {
+      console.log("Some Problem")
+    }
+  })
+  next();
+});
+
+app.get("", (req, res) => {
+  res.render("index", {
+    welcomeMessage: 'Welcome! You are at home page.',
+    footerMessage: 'home page Footer',
+    author: 'GokuNik',
+    body: 'Marks anaylsis',
+    data: {
+      sem1: {
+        sem: 'Semester 1',
+        tgpa: 10
+      },
+      sem2: {
+        sem: 'Semester 2',
+        tgpa: 10
+      },
+      sem3: {
+        sem: 'Semester 3',
+        tgpa: 10
+      },
+      sem4: {
+        sem: 'Semester 4',
+        tgpa: 10
+      },
+      sem5: {
+        sem: 'Semester 5',
+        tgpa: 10
+      }
+    }
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
+app.get("/about", (req, res) => {
+  res.render("about", {
+    welcomeMessage: 'Welcome! You are at About page.',
+    footerMessage: 'About page Footer',
+    author: 'Nik',
+    body: 'Know more about us'
   });
 });
 
-app.get('/updation', (req, res) => {
-  res.render('updation.hbs', {
-    pageTitle: 'UpDation'
+app.get("/help", (req, res) => {
+  res.render("help", {
+    welcomeMessage: 'Welcome! You are at help page.',
+    footerMessage: 'help page Footer',
+    author: 'vegita',
+    body: 'Need any assistance?',
   });
 });
 
-// /bad - send back json with errorMessage
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
+app.get('*', (req, res) => {
+  res.send('No such page')
+})
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
+app.listen(port);
+{
+  console.log("Server running succesfully at ", port);
+}
